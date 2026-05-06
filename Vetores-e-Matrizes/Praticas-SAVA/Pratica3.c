b4b33b8b212d31bc7084be4eee0d5301d35c435f
Daniel Lima <91384776+DanielLima1204@users.noreply.github.com>
2026-05-06 14:21:10 -0300

    Resolução Prática 3 de Vetores e Matrizes no SAVA.

---
+++ Vetores-e-Matrizes/Praticas-SAVA/Pratica3.c
@@ -0,0 +1,37 @@
+// Prática 3
+// Leia uma sequência de letras, terminada na letra (”z”), e mostre
+// quantas vezes cada vogal (a, e, i, o, u) apareceu, no emulador a
+// seguir.
+
+#include <stdio.h>
+
+int main() {
+    int quantidadeDeVogais[5] = {0, 0, 0, 0, 0};
+    char letraDigitada;
+    
+    do {
+        printf("Digite uma letra: ");
+        scanf(" %c", &letraDigitada);
+        switch(letraDigitada){
+            case 'a':
+                quantidadeDeVogais[0] = quantidadeDeVogais[0] + 1;
+                break;
+            case 'e':
+                quantidadeDeVogais[1] = quantidadeDeVogais[1] + 1;
+                break;
+            case 'i':
+                quantidadeDeVogais[2] = quantidadeDeVogais[2] + 1;
+                break;
+            case 'o':
+                quantidadeDeVogais[3] = quantidadeDeVogais[3] + 1;
+                break;
+            case 'u':
+                quantidadeDeVogais[4] = quantidadeDeVogais[4] + 1;
+                break;    
+        }
+       
+    } while((letraDigitada != 'z') && (letraDigitada != 'Z'));
+    printf("A quantide de vogais e = a: %d, e: %d, i: %d, o: %d, u: %d \n", quantidadeDeVogais[0], quantidadeDeVogais[1], quantidadeDeVogais[2], quantidadeDeVogais[3], quantidadeDeVogais[4]);
+    
+    return 0;
+}
