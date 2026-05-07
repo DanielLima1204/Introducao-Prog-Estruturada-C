46f82dd06c08ba09badad6326b0cc3e52382c343
Daniel Lima <91384776+DanielLima1204@users.noreply.github.com>
2026-05-07 13:06:57 -0300

    Resolução questão 9 da lista 5.

--- LISTAS/Lista5/exec9.c
+++ LISTAS/Lista5/exec9.c
@@ -2,10 +2,20 @@
 
 int main() {
     int nota, continuar;
-    int quantidade = 0; soma = 0;
+    int quantidade = 0, soma = 0;
     float media;
+    
     do {
-        
+        printf("Digite uma nota entre 1 (nada satisfeito) a 5 (muito satisfeito): ");
+        scanf("%d", &nota);
+        if(nota >= 1 && nota <= 5) {
+            quantidade++;
+            soma += nota;
+        } else {
+           printf("NOTA INVALIDA!\n"); 
+        }
+        printf("Deseja Continuar? 1 - SIM e 0 - NÃO \n");
+        scanf("%d", &continuar);
     } while(continuar != 0);
     
     if(quantidade > 0) {
