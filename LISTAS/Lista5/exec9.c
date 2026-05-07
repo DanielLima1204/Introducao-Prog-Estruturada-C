ea7db90173a1ad4f5b7f515dbda91d8d112438ea
Daniel Lima <91384776+DanielLima1204@users.noreply.github.com>
2026-05-07 05:28:14 -0300

    Inicio da resolucao da questao 9 litsa 5 (ainda devo finalizar).

---
+++ LISTAS/Lista5/exec9.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int nota, continuar;
+    int quantidade = 0; soma = 0;
+    float media;
+    do {
+        
+    } while(continuar != 0);
+    
+    if(quantidade > 0) {
+      media = (float) soma / quantidade;
+      printf("Media: %.2f\n", media);
+    } else {
+        printf("Nenhuma nota valida registrada.\n");    
+    }
+
+    return 0;
+}
