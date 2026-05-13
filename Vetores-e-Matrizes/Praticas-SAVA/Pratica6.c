3b63aad0e4552dbc8f8d8a239449612d57c11bc4
Daniel Lima <91384776+DanielLima1204@users.noreply.github.com>
2026-05-13 14:33:23 -0300

    Inicio resolução prática 5 vetores no SAVA.

---
+++ Vetores-e-Matrizes/Praticas-SAVA/Pratica6.c
@@ -0,0 +1,19 @@
+///Prática 6
+// Considere o seguinte problema em uma escola primária: 
+// em uma turma com 50 alunos, cada um faz 3 provas por semestre.
+// Além de armazenar as 3 provas dos 50 alunos, existe a necessidade de mostrar:
+// A média de cada prova;
+// A média de cada aluno;
+// A média da turma.
+
+#include <stdio.h>
+
+int main() {
+    int alunos[10];
+    int notasPrimeiraProva[10];
+    int notasSegundoProva[10];
+    
+    
+    
+    return 0;
+}
