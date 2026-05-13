9b8394d44857d6dc60cad8c4b2b68d27a5715261
Daniel Lima <91384776+DanielLima1204@users.noreply.github.com>
2026-05-13 19:31:43 -0300

    Resolução questão 3 lista 6.

---
+++ LISTAS/Lista6/exec3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int numeros[10];
+    int pos, posicao, maior = 0;
+    for(pos = 0; pos < 10; pos++) {
+       printf("Digite o %dº numero: ", pos+1);
+       scanf("%d", &numeros[pos]);
+       if(numeros[pos] > maior) {
+           maior = numeros[pos];
+           posicao = pos+1;
+           
+       }
+    }
+    printf("O maior numero digitado é: %d na posição: %dº", maior, posicao);
+    
+    return 0;
+}
